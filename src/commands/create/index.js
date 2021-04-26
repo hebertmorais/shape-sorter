3f248375ba50abab3c78db09509dc4f21852f097
Hebert Morais <hebertlucashllm@gmail.com>
2021-04-26 08:37:27 -0300

    feat: make questions about dependencies

--- src/commands/create/index.js
+++ src/commands/create/index.js
@@ -1,7 +1,65 @@
 import prompts from "prompts";
-import { defaultQuestions } from "./questions";
+import {
+  defaultQuestions,
+  isDependencyEagerQuestion,
+  isDependencySingletonQuestion,
+  isDependencyVersionDifferentQuestion,
+} from "./questions";
+import mockPackage from "../../mocks/package.json";
+
+const dependencies = mockPackage.dependencies;
+const dependenciesKeys = Object.keys(dependencies);
 
 (async () => {
-  const answers = await prompts(defaultQuestions);
-  console.log(answers);
+  const answers = await prompts(defaultQuestions(dependenciesKeys));
+  const mfName = answers["mf-name"];
+  const mfFileName = answers["mf-file-name"];
+  const mfType = answers["mf-type"];
+  const mfLanguage = answers["mf-language"];
+  const mfSharedDependencies = answers["mf-shared-dependencies"];
+  const versionAnswers = await prompts(
+    makeDependenciesQuestions(dependencies, mfSharedDependencies)
+  );
+  console.log(answers, versionAnswers);
 })();
+
+function makeDependenciesQuestions(packageDependencies, mfSharedDependencies) {
+  const sharedDependenciesSingletonQuestions = [];
+  mfSharedDependencies.map((sharedDependency) => {
+    sharedDependenciesSingletonQuestions.push(
+      isDependencySingletonQuestion(sharedDependency)
+    );
+  });
+
+  const sharedDependenciesEagerQuestions = [];
+  mfSharedDependencies.map((sharedDependency) => {
+    sharedDependenciesEagerQuestions.push(
+      isDependencyEagerQuestion(sharedDependency)
+    );
+  });
+
+  const sharedDependenciesVersionQuestions = [];
+  mfSharedDependencies.map((sharedDependency) => {
+    sharedDependenciesVersionQuestions.push(
+      isDependencyVersionDifferentQuestion(
+        sharedDependency,
+        packageDependencies[sharedDependency]
+      )
+    );
+  });
+  const allDependenciesQuestions = [
+    sharedDependenciesVersionQuestions,
+    sharedDependenciesEagerQuestions,
+    sharedDependenciesSingletonQuestions,
+  ];
+
+  //rotate matrix so it make all the questions about each dependency at a time
+  const questionsRotatedMatrix = allDependenciesQuestions[0].map((_, index) =>
+    allDependenciesQuestions.map((row) => row[index]).reverse()
+  );
+
+  //turning the 2d matrix into 1d array
+  const dependenciesQuestionsRightOrder = [].concat(...questionsRotatedMatrix);
+
+  return dependenciesQuestionsRightOrder;
+}
