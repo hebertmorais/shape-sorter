c490bc7ea25a853c0d383949f16dcf5dd528010a
Hebert Morais <hebertlucashllm@gmail.com>
2021-05-12 10:49:22 -0300

    feat: add remote component in remotes dir

--- src/utils/file.js
+++ src/utils/file.js
@@ -3,15 +3,30 @@ import * as path from "path";
 import AdmZip from "adm-zip";
 
 export const unzipToDirectory = (srcZip, destFolder) => {
-  const zipPath = `${path.join(__dirname, srcZip)}`;
+  const zipPath = getFolderDirectoryPath(srcZip);
   const zip = new AdmZip(zipPath);
   zip.extractAllTo(destFolder, true);
 };
 
-export const openFile = (path) => {
-  return fs.readFileSync(path);
+export const openFile = (path, options = {}) => {
+  return fs.readFileSync(path, options);
 };
 
 export const writeDataInFile = (path, data) => {
   return fs.writeFileSync(path, data);
+};
+
+export const addFileToDirectory = (dirPath, fileName, fileContent) => {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+  }
+  writeDataInFile(`${dirPath}/${fileName}`, fileContent);
+};
+
+export const getFolderDirectoryPath = (directory) => {
+  return `${path.join(__dirname, directory)}`;
+};
+
+export const getSafeFileNameFromString = (name) => {
+  return name.replace(/[^a-z0-9]/gi, "_").toLowerCase();
 };
--- src/utils/templates.js
+++ src/utils/templates.js
@@ -1,8 +1,35 @@
-import { unzipToDirectory } from "../utils/file";
+import {
+  unzipToDirectory,
+  openFile,
+  getFolderDirectoryPath,
+  addFileToDirectory,
+  getSafeFileNameFromString,
+} from "../utils/file";
 
 const currentDir = process.cwd();
 const reactTemplateZipPath = "../../templates/react.zip";
+const remoteTemplatePath = "../../templates/remote.txt";
 
 export const copyReactTemplate = () => {
   unzipToDirectory(reactTemplateZipPath, currentDir);
+};
+
+export const createRemoteTemplate = (remoteName, moduleName) => {
+  let templateContent = openFile(getFolderDirectoryPath(remoteTemplatePath), {
+    encoding: "utf8",
+    flag: "r",
+  });
+  templateContent = templateContent
+    .replace(/{{componentName}}/g, `${moduleName}RemoteComponent`)
+    .replace(/{{moduleImport}}/g, `${remoteName}/${moduleName}`)
+    .replace(/{{moduleName}}/g, `${remoteName}${moduleName}RemoteComponent`);
+
+  const remoteTemplateDestDir = `${currentDir}/src/remotes`;
+  const fileName = getSafeFileNameFromString(`${remoteName}/${moduleName}`);
+  const fileExtension = "js";
+  addFileToDirectory(
+    remoteTemplateDestDir,
+    `${fileName}.${fileExtension}`,
+    templateContent
+  );
 };
