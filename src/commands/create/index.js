377c422e9d9ee94d81a0a08ab6ff112b454f594c
Hebert Morais <hebertlucashllm@gmail.com>
2021-05-12 17:45:31 -0300

    refactor: remove dependency questions from create and add formatting

--- src/commands/create/index.js
+++ src/commands/create/index.js
@@ -1,116 +1,44 @@
 import prompts from "prompts";
-import {
-  defaultQuestions,
-  isDependencyEagerQuestion,
-  isDependencySingletonQuestion,
-  isDependencyVersionDifferentQuestion,
-} from "./questions";
+import defaultQuestions from "./questions";
 import { writeDataInFile } from "../../utils/file";
 import { copyReactTemplate } from "../../utils/templates";
 
-function makeDependenciesQuestions(packageDependencies, mfSharedDependencies) {
-  const sharedDependenciesSingletonQuestions = [];
-  mfSharedDependencies.map((sharedDependency) => {
-    sharedDependenciesSingletonQuestions.push(
-      isDependencySingletonQuestion(sharedDependency)
-    );
-  });
-
-  const sharedDependenciesEagerQuestions = [];
-  mfSharedDependencies.map((sharedDependency) => {
-    sharedDependenciesEagerQuestions.push(
-      isDependencyEagerQuestion(sharedDependency)
-    );
-  });
-
-  const sharedDependenciesVersionQuestions = [];
-  mfSharedDependencies.map((sharedDependency) => {
-    sharedDependenciesVersionQuestions.push(
-      isDependencyVersionDifferentQuestion(
-        sharedDependency,
-        packageDependencies[sharedDependency]
-      )
-    );
-  });
-
-  const allDependenciesQuestions = [
-    sharedDependenciesVersionQuestions,
-    sharedDependenciesEagerQuestions,
-    sharedDependenciesSingletonQuestions,
-  ];
-
-  //rotate matrix so it make all the questions about each dependency at a time
-  const questionsRotatedMatrix = allDependenciesQuestions[0].map((_, index) =>
-    allDependenciesQuestions.map((row) => row[index]).reverse()
-  );
-
-  //turning the 2d matrix into 1d array
-  const dependenciesQuestionsRightOrder = [].concat(...questionsRotatedMatrix);
-
-  return dependenciesQuestionsRightOrder;
-}
-
-const buildShapeSorterConfig = ({
-  mfName,
-  mfType,
-  mfPort,
-  mfBuiltSharedDependencies,
-}) => {
+const buildShapeSorterConfig = ({ mfName, mfType, mfPort }) => {
   return {
     name: mfName,
     remotes: {},
-    exposes: {},
+    exposes: { "./Main": `./src/components/Main.js` },
+    filename: "remoteEntry.js",
     type: mfType,
     port: mfPort,
-    shared: mfBuiltSharedDependencies,
+    shared: {
+      react: {
+        singleton: true,
+        eager: mfType == "host",
+      },
+      "react-dom": {
+        singleton: true,
+        eager: mfType == "host",
+      },
+    },
   };
 };
 
-const buildSharedDependencies = (mfDependencies, mfDependenciesOpts) => {
-  let depObj = {};
-  mfDependencies.map((dependency) => {
-    depObj[dependency] = {
-      singleton:
-        mfDependenciesOpts[`is-singleton-${dependency}`] !== "" || false,
-      eager: mfDependenciesOpts[`is-eager-${dependency}`] !== "" || false,
-      requiredVersion:
-        mfDependenciesOpts[`is-different-version-${dependency}`] ||
-        dependencies[dependency],
-    };
-  });
-  return depObj;
-};
-
-copyReactTemplate();
-const pck = require(`${process.cwd()}/package.json`);
-const dependencies = pck.dependencies;
-const dependenciesKeys = Object.keys(dependencies);
-
 const runCreate = async () => {
-  const answers = await prompts(defaultQuestions(dependenciesKeys));
+  const answers = await prompts(defaultQuestions);
   const mfName = answers["mf-name"];
   const mfType = answers["mf-type"];
   const mfPort = answers["mf-port"];
-  const mfSharedDependencies = answers["mf-shared-dependencies"];
-  const mfDependenciesOpts = await prompts(
-    makeDependenciesQuestions(dependencies, mfSharedDependencies)
-  );
-
-  const mfBuiltSharedDependencies = buildSharedDependencies(
-    mfSharedDependencies,
-    mfDependenciesOpts
-  );
 
   const shapeSorter = buildShapeSorterConfig({
     mfName,
     mfType,
     mfPort,
-    mfBuiltSharedDependencies,
   });
 
   const configPath = `${process.cwd()}/.shapesorterrc.json`;
-  const data = JSON.stringify(shapeSorter);
-
+  const data = JSON.stringify(shapeSorter, null, 4);
+  copyReactTemplate(mfType);
   writeDataInFile(configPath, data);
   console.log("Configuração criada com sucesso");
 };
--- src/commands/create/questions.js
+++ src/commands/create/questions.js
@@ -1,145 +1,31 @@
-export const defaultQuestions = (dependencies) => {
-  return [
-    {
-      type: "text",
-      name: "mf-name",
-      message: "What is the name of your micro frontend?",
-      validate: (name) =>
-        name === "" ? "The micro frontend name cannot be empty!" : true,
-    },
-    {
-      type: "select",
-      name: "mf-type",
-      message: "What is the type of your micro frontend?",
-      choices: [
-        {
-          title: "Host (Shell)",
-          value: "host",
-        },
-        {
-          title: "Module",
-          value: "module",
-        },
-      ],
-    },
-    {
-      type: "text",
-      name: "mf-port",
-      message: "Which port would you like to run your project?",
-    },
-    {
-      type: "multiselect",
-      name: "mf-shared-dependencies",
-      message:
-        "Which dependencies would you like to share between your micro frontends?",
-      choices: dependencies.map((dependency) => {
-        return { title: dependency, value: dependency };
-      }),
-      instructions: "\n- Space to select. Enter to submit",
-    },
-  ];
-};
-
-export const webpackVersionBelow5Question = {
-  type: "select",
-  name: "mf-webpack-version",
-  message:
-    "Your webpack version is below 5, which is the minimum required, what would you like to do?",
-  choices: [
-    {
-      title: "Update to the latest version",
-      value: "update",
-    },
-    {
-      title: "Continue anyway (not recommended)",
-      value: "continue",
-    },
-    {
-      title: "Quit",
-      value: "quit",
-    },
-  ],
-};
-
-export const webpackNotFoundQuestion = {
-  type: "select",
-  name: "mf-webpack-version",
-  message:
-    "Webpack config file not found. Would you like to install and continue",
-  choices: [
-    {
-      title: "Yes, install and proceed.",
-      value: "proceed",
-    },
-    {
-      title: "No, abort and quit",
-      value: "abort",
-    },
-  ],
-};
-
-export const isDependencySingletonQuestion = (dependency) => {
-  return {
-    type: "select",
-    name: `is-singleton-${dependency}`,
-    message: `The dependency ${dependency} is a singleton?`,
-    choices: [
-      {
-        title: "Yes",
-        value: "yes",
-      },
-      {
-        title: "No",
-        value: "no",
-      },
-    ],
-  };
-};
-
-export const isDependencyEagerQuestion = (dependency) => {
-  return {
-    type: "select",
-    name: `is-eager-${dependency}`,
-    message: `The dependency ${dependency} is a eager?`,
-    choices: [
-      {
-        title: "Yes",
-        value: "yes",
-      },
-      {
-        title: "No",
-        value: "no",
-      },
-    ],
-  };
-};
-
-export const isDependencyVersionDifferentQuestion = (
-  dependency,
-  dependencyVersion
-) => {
-  return {
+export const defaultQuestions = [
+  {
     type: "text",
-    name: `is-different-version-${dependency}`,
-    message: `The version of ${dependency} is different from ${dependencyVersion}? (If yes insert the version, if no press Enter)`,
-  };
-};
-
-export const modulesFederationExistsQuestion = {
-  type: "text",
-  name: "mf-webpack-federation",
-  message:
-    "The modules federation plugin has already been added, would you like to overwrite it? Y/N (Enter for no)",
-};
-
-const allQuestions = [
-  defaultQuestions,
-  isDependencyEagerQuestion,
-  isDependencySingletonQuestion,
-  isDependencyVersionDifferentQuestion,
-  modulesFederationExistsQuestion,
-  webpackNotFoundQuestion,
-  webpackVersionBelow5Question,
+    name: "mf-name",
+    message: "What is the name of your micro frontend?",
+    validate: (name) =>
+      name === "" ? "The micro frontend name cannot be empty!" : true,
+  },
+  {
+    type: "select",
+    name: "mf-type",
+    message: "What is the type of your micro frontend?",
+    choices: [
+      {
+        title: "Host (Shell)",
+        value: "host",
+      },
+      {
+        title: "Module",
+        value: "module",
+      },
+    ],
+  },
+  {
+    type: "text",
+    name: "mf-port",
+    message: "Which port would you like to run your project?",
+  },
 ];
 
-export default allQuestions;
+export default defaultQuestions;
